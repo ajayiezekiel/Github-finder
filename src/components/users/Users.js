dde4d29b3110954f528035a290007e32fd3c46bc
Ezekiel <ajayiezekiel9@gmail.com>
2021-01-23 20:07:56 +0100

    Api consumption and spinner component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,43 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/spinner.gif';
+import PropTypes from 'prop-types'
 
-class Users extends Component {
-    state = {
-        users: [
-            {
-                "login": "mojombo",
-                "id": 1,
-                "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
-                "url": "https://api.github.com/users/mojombo"  
-            },
-            {
-                "login": "defunkt",
-                "id": 2,
-                "avatar_url": "https://avatars.githubusercontent.com/u/2?v=4",
-                "html_url": "https://github.com/defunkt"
-            },
-            {
-                "login": "pjhyett",
-                "id": 3,
-                "avatar_url": "https://avatars.githubusercontent.com/u/3?v=4",
-                "html_url": "https://github.com/pjhyett"
-            }
-        ]
-    }
-    render() {
+
+const Users = ({users, loading}) => {
+    if (loading) {
+        <Spinner />
+    } else {
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => 
+                {users.map(user => 
                     (<UserItem key={user.id} user={user} />)
                 )}         
             </div>
         );
     }
+    
 }
+
+Users.proptypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+}
+
+
 const userStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 1fr)',
     gridGap: '1rem'
 }
+
 
 export default Users
